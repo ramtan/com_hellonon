b9ce0d78ff5ab568aba01f83a0f07ea19262a260
NnNnnNn <on-job@hotmail.com>
2016-02-10 14:53:09 +0700

    add new file type
    add new file type

--- site/models/hellonon.php
+++ site/models/hellonon.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Administrator
  * @subpackage  com_hellonon
  *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
  
@@ -11,44 +11,66 @@
 defined('_JEXEC') or die('Restricted access');
  
 /**
- * HelloNon Model
+ * HelloWorld Model
  *
  * @since  0.0.1
  */
 class HelloNonModelHelloNon extends JModelItem
 {
 	/**
-	 * @var string message
+	 * @var array messages
 	 */
-	protected $message;
+	protected $messages;
+ 
+	/**
+	 * Method to get a table object, load it if necessary.
+	 *
+	 * @param   string  $type    The table name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.
+	 *
+	 * @return  JTable  A JTable object
+	 *
+	 * @since   1.6
+	 */
+	public function getTable($type = 'HelloNon', $prefix = 'HelloNonTable', $config = array())
+	{
+		return JTable::getInstance($type, $prefix, $config);
+	}
  
 	/**
 	 * Get the message
-         *
-	 * @return  string  The message to be displayed to the user
+	 *
+	 * @param   integer  $id  Greeting Id
+	 *
+	 * @return  string        Fetched String from Table for relevant Id
 	 */
-	public function getNonMsg()
+	public function getNonMsg($id = 1)
 	{
-		if (!isset($this->message))
+		if (!is_array($this->messages))
 		{
+			$this->messages = array();
+		}
+ 
+		if (!isset($this->messages[$id]))
+		{
+			// Request the selected id
 			//ประกาศ getApp input เพื่อจะใช้ Method input ดึงค่า parameter
 			$jinput = JFactory::getApplication()->input;
 
 			//กำหนดดึงจาก input name='id' มี 1 ตัวและ มีค่าเป็น int
-			$id = $jinput->get('id', 1 ,'INT');
-
-			switch ($id)
-			{
-				case 2:
-					$this->message = 'Good bye World!';
-					break;
-				default:
-				case 1:
-					$this->message = 'Hello World!';
-					break;
-			}
+			$id     = $jinput->get('id', 1, 'INT');
+ 
+			// Get a TableHelloWorld instance
+			$table = $this->getTable();
+ 
+			// Load the message
+			$table->load($id);
+ 
+			// Assign the message
+			$this->messages[$id] = $table->greeting;
 		}
  
-		return $this->message;
+		return $this->messages[$id];
 	}
 }
