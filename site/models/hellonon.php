88fbe51341cd871ab2768947f7d2e78eb3aa74ca
NnNnnNn <on-job@hotmail.com>
2016-02-10 12:14:30 +0700

    Add model
    Add model

---
+++ site/models/hellonon.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_hellonon
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * HelloNon Model
+ *
+ * @since  0.0.1
+ */
+class HelloNonModelHelloNon extends JModelItem
+{
+	/**
+	 * @var string message
+	 */
+	protected $message;
+ 
+	/**
+	 * Get the message
+         *
+	 * @return  string  The message to be displayed to the user
+	 */
+	public function getMsg()
+	{
+		if (!isset($this->message))
+		{
+			$this->message = 'Hello Non Model!';
+		}
+ 
+		return $this->message;
+	}
+}
--- site/views/hellonon/view.html.php
+++ site/views/hellonon/view.html.php
@@ -27,7 +27,15 @@ class HelloNonViewHelloNon extends JViewLegacy
 	function display($tpl = null)
 	{
 		// Assign data to the view
-		$this->msg = 'You stand in frontend at Non Toolkit';
+		$this->msg = $this->get('Msg');
+		
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			JLog::add(implode('<br />', $errors), JLog::WARNING, 'jerror');
+ 
+			return false;
+		}
  
 		// Display the view
 		parent::display($tpl);
