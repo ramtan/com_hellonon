34f0f5b7c98f8cfb753fe048974271d1579aec6e
NnNnnNn <on-job@hotmail.com>
2016-02-11 11:53:01 +0700

    add 1st backend
    add 1st backend

---
+++ admin/controller.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_hellonon
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * General Controller of Hellonon component
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_hellonon
+ * @since       0.0.7
+ */
+class HelloNonController extends JControllerLegacy
+{
+	/**
+	 * The default view for the display method.
+	 *
+	 * @var string
+	 * @since 12.2
+	 */
+	protected $default_view = 'hellonon';
+}
